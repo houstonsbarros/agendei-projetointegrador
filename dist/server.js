696e85a017c8b5dcc1c3142b5e19d386d6089480
Houston Barros <capar.2022113TADS0170@aluno.ifpi.edu.br>
2023-06-17 13:39:35 -0300

    Alteração da porta

--- dist/server.js
+++ dist/server.js
@@ -6,13 +6,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var database_1 = require("./database");
 var routes_1 = require("./routes");
+var dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 var app = (0, express_1.default)();
 var cors = require('cors');
 app.use(cors());
 app.use(express_1.default.static('public'));
 app.use(express_1.default.json());
 app.use(routes_1.router);
-var port = process.env.PORT || 4000;
+var port = process.env.PORT;
 app.listen(port, function () {
     database_1.sequelize.authenticate().then(function () {
         console.log('Conectado ao Banco de Dados');
