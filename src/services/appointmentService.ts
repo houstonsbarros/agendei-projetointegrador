2d52b1f1ab95ecdd03a6de78d912e6612e1e2d90
Houston Barros <capar.2022113TADS0170@aluno.ifpi.edu.br>
2023-06-18 14:22:26 -0300

    Teste de Rotas

--- src/services/appointmentService.ts
+++ src/services/appointmentService.ts
@@ -93,7 +93,6 @@ export const appointmentService = {
       professionals p ON p.id = a.professional_id
     WHERE
       a.client_id = ${client_id}
-      AND a.status = 'Pendente'
       AND a.schedule->>'date' >= '${new Date().toISOString().slice(0, 10)}'
       GROUP BY a.id, c.id, p.id
       `,
