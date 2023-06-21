c2802cb07fbad66f2ba226b0c670e6486b81e2c8
Houston Barros <107281650+houstonsbarros@users.noreply.github.com>
2023-06-21 07:53:44 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,6 @@ const cors = require('cors');
 app.use(cors());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
