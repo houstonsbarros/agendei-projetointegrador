97e1b2f41648b69b5e3d9d0cfa3e01aa7666c573
Houston Barros <capar.2022113TADS0170@aluno.ifpi.edu.br>
2023-06-24 13:26:51 -0300

    Atualização do CORS

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,7 @@ const cors = require('cors');
 app.use(cors());
 
 app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
